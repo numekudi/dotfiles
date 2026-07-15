61b05f1bc8b8e1367551781e060105e505c96c26
n <numekudi@primitive-ojisan.com>
2026-07-15 09:18:35 +0900

    update

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,55 +1,59 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {},
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.api.nvim_create_autocmd("CursorHold", {
-        callback = function()
-          vim.diagnostic.open_float(nil, { focusable = false, scope = "cursor" })
-        end,
-      })
+			vim.api.nvim_create_autocmd("CursorHold", {
+				callback = function()
+					vim.diagnostic.open_float(nil, { focusable = false, scope = "cursor" })
+				end,
+			})
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local bufnr = args.buf
-          local map = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
-          end
-          map("gd", function() require("telescope.builtin").lsp_definitions() end, "Go to Definition")
-          map("K", vim.lsp.buf.hover, "Hover Documentation")
-          map("<leader>rn", vim.lsp.buf.rename, "Rename")
-          map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
-          map("<leader>f", function() vim.lsp.buf.format({ async = true }) end, "Format")
-          vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { buffer = bufnr, desc = "LSP: Rename" })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ async = false, bufnr = bufnr })
-            end,
-          })
-        end,
-      })
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local bufnr = args.buf
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
+					end
+					map("gd", function()
+						require("telescope.builtin").lsp_definitions()
+					end, "Go to Definition")
+					map("K", vim.lsp.buf.hover, "Hover Documentation")
+					map("<leader>rn", vim.lsp.buf.rename, "Rename")
+					map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+					map("<leader>f", function()
+						vim.lsp.buf.format({ async = true })
+					end, "Format")
+					vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { buffer = bufnr, desc = "LSP: Rename" })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false, bufnr = bufnr })
+						end,
+					})
+				end,
+			})
 
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "pyright" },
-        automatic_installation = true,
-        handlers = {
-          function(server_name)
-            lspconfig[server_name].setup({ capabilities = capabilities })
-          end,
-        },
-      })
-    end,
-  },
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright", "vtsls" },
+				automatic_installation = false,
+				handlers = {
+					function(server_name)
+						lspconfig[server_name].setup({ capabilities = capabilities })
+					end,
+				},
+			})
+		end,
+	},
 }
