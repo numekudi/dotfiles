e1aca52428b8f5692b0190dd197fe11840433cb1
n <numekudi@primitive-ojisan.com>
2026-07-16 22:48:56 +0900

    Update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,7 +32,7 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<CR><Esc>", { desc = "Save" })
 vim.keymap.set({ "n", "v" }, "H", "^", { desc = "Go to line start" })
 vim.keymap.set({ "n", "v" }, "L", "$", { desc = "Go to line end" })
-
+vim.keymap.set({ "n" }, "<Esc><Esc>", ":<C-u>set nohlsearch<Return>", { desc = "set nohlsearch" })
 -- Load plugins
 require("lazy").setup("plugins", {
   change_detection = { notify = false },
